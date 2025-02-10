06e8f15ce2eba2f0e5f8d83bbba5817123c4cc50
Sophia Chirskaya <sophia.chirskaya@gmail.com>
2025-02-10 16:34:00 +0100

    [feat]error 404

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const postsRouter = require('./routers/posts');
 // Importo il middleware di gestione errore server
 const errorHandler = require("./middlewares/errorsHandler");
 
+// Importo il middleware di gestione errore not found
+const notFound = require("./middlewares/notFound");
+
 // Definizione d'uso di una cartella per i file statici
 app.use(express.static('public'));
 
@@ -21,6 +24,9 @@ app.use("/posts", postsRouter)
 
 // Utilizzo il middleware di gestione errore server
 app.use(errorHandler);
+
+// Utilizzo il middleware di gestione errore 404 not found
+app.use(notFound);
 
 
 app.listen(port, () => {
